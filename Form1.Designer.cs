a3fdf59494a9f58d3d3c91623b191178f881ac05
Kirill Kireev <kereeevkirill@gmail.com>
2020-11-27 18:11:11 +0300

    Add Tools Button

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,13 +29,192 @@ namespace VectorDrawing
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.BrushButton = new System.Windows.Forms.Button();
+            this.LineButton = new System.Windows.Forms.Button();
+            this.NlineButton = new System.Windows.Forms.Button();
+            this.RectangleButton = new System.Windows.Forms.Button();
+            this.SquareButton = new System.Windows.Forms.Button();
+            this.CircleButton = new System.Windows.Forms.Button();
+            this.EllipseButton = new System.Windows.Forms.Button();
+            this.RectangularTriangleButton = new System.Windows.Forms.Button();
+            this.TriangleButton = new System.Windows.Forms.Button();
+            this.AlpelesTriangleButton = new System.Windows.Forms.Button();
+            this.PolygonButton = new System.Windows.Forms.Button();
+            this.RegularPolygonButton = new System.Windows.Forms.Button();
+            this.ToolsLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // BrushButton
+            // 
+            this.BrushButton.ImageAlign = System.Drawing.ContentAlignment.TopLeft;
+            this.BrushButton.Location = new System.Drawing.Point(11, 67);
+            this.BrushButton.Name = "BrushButton";
+            this.BrushButton.Size = new System.Drawing.Size(171, 26);
+            this.BrushButton.TabIndex = 0;
+            this.BrushButton.Text = "Кисть";
+            this.BrushButton.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.BrushButton.UseVisualStyleBackColor = true;
+            // 
+            // LineButton
+            // 
+            this.LineButton.Location = new System.Drawing.Point(11, 102);
+            this.LineButton.Name = "LineButton";
+            this.LineButton.Size = new System.Drawing.Size(171, 28);
+            this.LineButton.TabIndex = 1;
+            this.LineButton.Text = "Линия";
+            this.LineButton.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.LineButton.UseVisualStyleBackColor = true;
+            // 
+            // NlineButton
+            // 
+            this.NlineButton.Location = new System.Drawing.Point(11, 136);
+            this.NlineButton.Name = "NlineButton";
+            this.NlineButton.Size = new System.Drawing.Size(171, 26);
+            this.NlineButton.TabIndex = 2;
+            this.NlineButton.Text = "Кривая линия";
+            this.NlineButton.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.NlineButton.UseVisualStyleBackColor = true;
+            // 
+            // RectangleButton
+            // 
+            this.RectangleButton.Location = new System.Drawing.Point(12, 168);
+            this.RectangleButton.Name = "RectangleButton";
+            this.RectangleButton.Size = new System.Drawing.Size(170, 26);
+            this.RectangleButton.TabIndex = 3;
+            this.RectangleButton.Text = "Прямоугольник";
+            this.RectangleButton.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.RectangleButton.UseVisualStyleBackColor = true;
+            // 
+            // SquareButton
+            // 
+            this.SquareButton.Location = new System.Drawing.Point(11, 200);
+            this.SquareButton.Name = "SquareButton";
+            this.SquareButton.Size = new System.Drawing.Size(171, 26);
+            this.SquareButton.TabIndex = 4;
+            this.SquareButton.Text = "Квадрат";
+            this.SquareButton.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.SquareButton.UseVisualStyleBackColor = true;
+            // 
+            // CircleButton
+            // 
+            this.CircleButton.Location = new System.Drawing.Point(12, 232);
+            this.CircleButton.Name = "CircleButton";
+            this.CircleButton.Size = new System.Drawing.Size(170, 26);
+            this.CircleButton.TabIndex = 5;
+            this.CircleButton.Text = "Круг";
+            this.CircleButton.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.CircleButton.UseVisualStyleBackColor = true;
+            // 
+            // EllipseButton
+            // 
+            this.EllipseButton.Location = new System.Drawing.Point(12, 264);
+            this.EllipseButton.Name = "EllipseButton";
+            this.EllipseButton.Size = new System.Drawing.Size(170, 26);
+            this.EllipseButton.TabIndex = 6;
+            this.EllipseButton.Text = "Эллипс";
+            this.EllipseButton.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.EllipseButton.UseVisualStyleBackColor = true;
+            // 
+            // RectangularTriangleButton
+            // 
+            this.RectangularTriangleButton.Location = new System.Drawing.Point(12, 296);
+            this.RectangularTriangleButton.Name = "RectangularTriangleButton";
+            this.RectangularTriangleButton.Size = new System.Drawing.Size(170, 26);
+            this.RectangularTriangleButton.TabIndex = 7;
+            this.RectangularTriangleButton.Text = "Прямоугольный треугольник";
+            this.RectangularTriangleButton.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.RectangularTriangleButton.UseVisualStyleBackColor = true;
+            // 
+            // TriangleButton
+            // 
+            this.TriangleButton.Location = new System.Drawing.Point(12, 328);
+            this.TriangleButton.Name = "TriangleButton";
+            this.TriangleButton.Size = new System.Drawing.Size(170, 26);
+            this.TriangleButton.TabIndex = 8;
+            this.TriangleButton.Text = "Треугольник";
+            this.TriangleButton.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.TriangleButton.UseVisualStyleBackColor = true;
+            // 
+            // AlpelesTriangleButton
+            // 
+            this.AlpelesTriangleButton.Location = new System.Drawing.Point(12, 360);
+            this.AlpelesTriangleButton.Name = "AlpelesTriangleButton";
+            this.AlpelesTriangleButton.Size = new System.Drawing.Size(170, 26);
+            this.AlpelesTriangleButton.TabIndex = 9;
+            this.AlpelesTriangleButton.Text = "Равнобедренный трегольник";
+            this.AlpelesTriangleButton.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.AlpelesTriangleButton.UseVisualStyleBackColor = true;
+            // 
+            // PolygonButton
+            // 
+            this.PolygonButton.Location = new System.Drawing.Point(12, 392);
+            this.PolygonButton.Name = "PolygonButton";
+            this.PolygonButton.Size = new System.Drawing.Size(170, 26);
+            this.PolygonButton.TabIndex = 10;
+            this.PolygonButton.Text = "Многоугольник";
+            this.PolygonButton.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.PolygonButton.UseVisualStyleBackColor = true;
+            // 
+            // RegularPolygonButton
+            // 
+            this.RegularPolygonButton.Location = new System.Drawing.Point(12, 424);
+            this.RegularPolygonButton.Name = "RegularPolygonButton";
+            this.RegularPolygonButton.Size = new System.Drawing.Size(170, 26);
+            this.RegularPolygonButton.TabIndex = 11;
+            this.RegularPolygonButton.Text = "Правильный многоугольник";
+            this.RegularPolygonButton.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.RegularPolygonButton.UseVisualStyleBackColor = true;
+            // 
+            // ToolsLabel
+            // 
+            this.ToolsLabel.AutoSize = true;
+            this.ToolsLabel.Font = new System.Drawing.Font("Vladimir Script", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ToolsLabel.Location = new System.Drawing.Point(12, 38);
+            this.ToolsLabel.Name = "ToolsLabel";
+            this.ToolsLabel.Size = new System.Drawing.Size(158, 26);
+            this.ToolsLabel.TabIndex = 12;
+            this.ToolsLabel.Text = "Инструменты";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(955, 572);
+            this.Controls.Add(this.ToolsLabel);
+            this.Controls.Add(this.RegularPolygonButton);
+            this.Controls.Add(this.PolygonButton);
+            this.Controls.Add(this.AlpelesTriangleButton);
+            this.Controls.Add(this.TriangleButton);
+            this.Controls.Add(this.RectangularTriangleButton);
+            this.Controls.Add(this.EllipseButton);
+            this.Controls.Add(this.CircleButton);
+            this.Controls.Add(this.SquareButton);
+            this.Controls.Add(this.RectangleButton);
+            this.Controls.Add(this.NlineButton);
+            this.Controls.Add(this.LineButton);
+            this.Controls.Add(this.BrushButton);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button BrushButton;
+        private System.Windows.Forms.Button LineButton;
+        private System.Windows.Forms.Button NlineButton;
+        private System.Windows.Forms.Button RectangleButton;
+        private System.Windows.Forms.Button SquareButton;
+        private System.Windows.Forms.Button CircleButton;
+        private System.Windows.Forms.Button EllipseButton;
+        private System.Windows.Forms.Button RectangularTriangleButton;
+        private System.Windows.Forms.Button TriangleButton;
+        private System.Windows.Forms.Button AlpelesTriangleButton;
+        private System.Windows.Forms.Button PolygonButton;
+        private System.Windows.Forms.Button RegularPolygonButton;
+        private System.Windows.Forms.Label ToolsLabel;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -16,5 +16,7 @@ namespace VectorDrawing
         {
             InitializeComponent();
         }
+
+       
     }
 }
