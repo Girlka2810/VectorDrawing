18f67832fd1a79890c383a527fe4e8ff11778c65
Чагай Сергей <frei.mizantrop@gmail.com>
2020-12-03 00:29:06 +0300

    Add CircleTool

---
+++ Figures/CircleFigure.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VectorDrawing.Figures
+{
+    class CircleFigure : IFigure
+    {
+        public Point[] GetPoints(FigureParameter parameter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Rectangle GetCircumscribedRectangle(FigureParameter parameter)
+        {
+            Point prev = parameter.Points[0];
+            Point crnt = parameter.TemporaryPoint;
+            int deltaX = Math.Abs(prev.X - crnt.X);
+            int deltaY = Math.Abs(prev.Y - crnt.Y);
+            int radius = (int) Math.Sqrt(deltaY * deltaY + deltaX * deltaX);
+            Size size = new Size(radius*2, radius*2);
+            Point center = new Point(prev.X - radius, prev.Y - radius);
+            Rectangle rectangle = new Rectangle(center, size);
+            return rectangle;
+        }
+    }
+}
---
+++ Tools/CircleTool.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VectorDrawing.Tools
+{
+    class CircleTool : AbstractTool
+    {
+        public override int MaxCount => 2;
+
+        public CircleTool(Pen pen) : base(pen)
+        {
+
+        }
+
+        public override void Paint(Graphics graphics)
+        {
+            Figures.CircleFigure circle = new Figures.CircleFigure();
+            Figures.FigureParameter figureParameter = new Figures.FigureParameter
+            {
+                Points = this.Points.ToArray(),
+                TemporaryPoint = this.TemporaryPoint
+            };
+            graphics.DrawEllipse(Pen, circle.GetCircumscribedRectangle(figureParameter));
+        }
+
+    }
+}
