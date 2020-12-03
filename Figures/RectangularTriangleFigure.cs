68877fded2630e8f59237b186fa49d89d990e3e3
Alexander <46569007+ProgWidness@users.noreply.github.com>
2020-12-03 13:49:10 +0300

    Сделал классы

---
+++ Figures/RectangularTriangleFigure.cs
@@ -0,0 +1,12 @@
+﻿using System.Drawing;
+
+namespace VectorDrawing.Figures
+{
+    public class RectangularTriangleFigure : IFigure
+    {
+        public Point[] GetPoints(FigureParameter parameter)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ Tools/RectangularTriangleTool.cs
@@ -0,0 +1,18 @@
+﻿using System.Drawing;
+
+namespace VectorDrawing.Tools
+{
+    public class RectangularTriangleTool : AbstractTool
+    {
+        public override int MaxCount { get; } = 2;
+        
+        public RectangularTriangleTool(Pen pen) : base(pen)
+        {
+        }
+        
+        public override void Paint(Graphics graphics)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
