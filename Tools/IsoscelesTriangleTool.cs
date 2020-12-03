1403c784e53c9457f268069bf352a18482cf4be7
Alexander <46569007+ProgWidness@users.noreply.github.com>
2020-12-03 16:55:28 +0300

    Сделал тул

---
+++ Tools/IsoscelesTriangleTool.cs
@@ -0,0 +1,18 @@
+﻿using System.Drawing;
+
+namespace VectorDrawing.Tools
+{
+    public class IsoscelesTriangleTool : AbstractTool
+    {
+        public override int MaxCount { get; } = 2;
+        public IsoscelesTriangleTool(Pen pen) : base(pen)
+        {
+        }
+
+        
+        public override void Paint(Graphics graphics)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
