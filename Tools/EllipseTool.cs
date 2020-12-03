ccd43de5049255d46e8d2382ad68a96754392aed
Kirill Kireev <kereeevkirill@gmail.com>
2020-12-03 16:38:49 +0300

    EllipseTool (No Test)

---
+++ Tools/EllipseTool.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VectorDrawing.Figures;
+
+namespace VectorDrawing.Tools
+{
+    class EllipseTool : AbstractTool
+    {
+        public override int MaxCount => 2;
+
+        public override void Paint(Graphics graphics)
+        {
+            EllipseFigure ellipseFigure = new EllipseFigure();
+            FigureParameter figureParameter = new FigureParameter
+            {
+                Points = Points.ToArray(),
+                TemporaryPoint = TemporaryPoint,
+            };
+            graphics.DrawPolygon(Pen, ellipseFigure.GetPoints(figureParameter));
+        }
+        public EllipseTool(Pen pen) : base(pen)
+        {
+        }
+    }
+}
--- VectorDrawingForm.cs
+++ VectorDrawingForm.cs
@@ -57,7 +57,7 @@ namespace VectorDrawing
                     _tool = null;
                     break;
                 case "Ellipse":
-                    _tool = null;
+                    _tool = new EllipseTool(_pen);
                     break;
                 case "Rectangular":
                     _tool = new RectangularTriangleTool(_pen);
