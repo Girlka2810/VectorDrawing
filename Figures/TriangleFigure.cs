06fa80041a8a7cdf35c7b65839273c6c5c7ba942
Alexander <46569007+ProgWidness@users.noreply.github.com>
2020-12-03 16:16:41 +0300

    Конфликты

---
+++ Figures/TriangleFigure.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Drawing;
+
+namespace VectorDrawing.Figures
+{
+    public class TriangleFigure : IFigure
+    {
+        public Point[] GetPoints(FigureParameter parameter)
+        {
+            if(parameter.Points.Length==0) throw  new NullReferenceException("Points count can't be null");
+            if (parameter.Points.Length < 3)
+            {
+                Point[] tmp = new Point[parameter.Points.Length+1];
+                Array.Copy(parameter.Points, tmp, parameter.Points.Length);
+                tmp[tmp.Length - 1] = parameter.TemporaryPoint;
+                return tmp;
+            }
+            
+            return parameter.Points;
+            
+        }
+    }
+}
