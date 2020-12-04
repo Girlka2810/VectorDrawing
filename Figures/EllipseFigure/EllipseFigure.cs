5bb6e30f3ea29af2aa5527b0cdae6fce877c2c61
Kirill Kireev <kereeevkirill@gmail.com>
2020-12-05 01:33:57 +0300

    Add EllipseFigureTests

--- Figures/EllipseFigure/EllipseFigure.cs
+++ Figures/EllipseFigure/EllipseFigure.cs
@@ -25,7 +25,6 @@ namespace VectorDrawing.Figures
             {
                 delX = Math.Abs(point.X - pointEnd.X);
                 delY = Math.Abs(point.Y - pointEnd.Y);
-                // upperLeftPoint = new Point(point.X - (int)delX, point.Y - (int)delY);
                 if (point.Y > pointEnd.Y)
                 {
                     upperLeftPoint = new Point(point.X - (int)delX, point.Y - (int)delY);
@@ -48,88 +47,12 @@ namespace VectorDrawing.Figures
                     upperLeftPoint = new Point(pointEnd.X - (int)delX, point.Y - (int)delY);
                 }
             }
-
-            ////1 - st quarter
-            //if (pointEnd.X < point.X && pointEnd.Y < point.Y)
-            //{
-            //    //upperLeftPoint = new Point(pointEnd.X, pointEnd.Y);
-
-            //    upperLeftPoint = new Point(point.X - (point.X - pointEnd.X), point.Y - (point.Y - pointEnd.Y));
-            //}
-            // 2 - nd quarter
-            //if (pointEnd.X > point.X && pointEnd.Y < point.Y)
-            //{
-            //    //upperLeftPoint = new Point(point.X, pointEnd.Y);
-
-            //    upperLeftPoint = new Point(pointEnd.X - ( pointEnd.X-point.X) , pointEnd.Y - (pointEnd.Y - point.Y)/((pointEnd.Y - point.Y)/2) );
-            //    //pointEnd = point;
-            //    //point = upperLeftPoint;
-            //}
-            // 3 - d quarter
-            //if (pointEnd.X < point.X && pointEnd.Y > point.Y)
-            //{
-            //    //upperLeftPoint = new Point(pointEnd.X, pointEnd.Y);
-
-            //    upperLeftPoint = new Point(pointEnd.X - (pointEnd.X - point.X) / ((pointEnd.X - point.X) / 2), pointEnd.Y - (pointEnd.Y - point.Y) );
-            //}
-            //if (pointEnd.X > point.X && pointEnd.Y > point.Y)
-            //{
-            //    // upperLeftPoint = new Point(point.X, point.Y);
-
-            //    upperLeftPoint = new Point(pointEnd.X - (pointEnd.X - point.X) / ((pointEnd.X - point.X) / 2), pointEnd.Y - (pointEnd.Y - point.Y) / ((pointEnd.Y - point.Y) / 2));
-            //}
-
-            // upperLeftPoint = new Point(point.X +(int) delX/2, point.Y + (int)delY/2);
-
-            //double r = Math.Sqrt(Math.Pow(delX, 2) + Math.Pow(delY, 2));
-            //// points = new Point[];
-            ////for (int i = 1; i <= 50; i++)
-            ////{
-            //double angle = Math.PI * 2 / 50 * i;
-            //int xd = Convert.ToInt32((delX * Math.Cos(angle) + point.X)); // эллипс
-            //int yd = Convert.ToInt32((delY * Math.Sin(angle) + point.Y));
-            //Point addPoint = new Point(xd, yd);
-            //}
             Rectangle rectangle = new Rectangle(upperLeftPoint.X, upperLeftPoint.Y, (int)delX , (int)delY);
-
-
             return rectangle;
         }
+
+       
     }
 }
-//    class EllipseFigure : IFigure
-//    {
-//        public Point[] GetPoints(FigureParameter parameter)
-//        {
-//            Point prev = parameter.Points[0];
-//            Point crnt = parameter.TemporaryPoint;
-//            List<Point> points = new List<Point>();
 
-
-//            int delx;
-//            int dely;
-//            if (prev.X >= crnt.X)
-//            {
-//                delx = Math.Abs(prev.X - crnt.X);
-//                dely = Math.Abs(prev.Y - crnt.Y);
-//            }
-//            else
-//            {
-//                delx = Math.Abs(crnt.X - prev.X);
-//                dely = Math.Abs(crnt.Y - prev.Y);
-//            }
-//            double r = Math.Sqrt(Math.Pow(delx, 2) + Math.Pow(dely, 2));
-//            // points = new Point[];
-//            for (int i = 1; i <= 50; i++)
-//            {
-//                double angle = Math.PI * 2 / 50 * i;
-//                int xd = Convert.ToInt32((delx * Math.Cos(angle) + prev.X)); // эллипс
-//                int yd = Convert.ToInt32((dely * Math.Sin(angle) + prev.Y));
-//                Point addPoint = new Point(xd, yd);
-//                points.Add(addPoint);
-//            }
-//            return points.ToArray();
-//        }
-//    }
-//}
 
