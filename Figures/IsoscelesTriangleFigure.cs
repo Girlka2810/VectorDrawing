cf0ef144062516086b741ae0d256631ef82ec392
Alexander <46569007+ProgWidness@users.noreply.github.com>
2020-12-08 19:52:11 +0300

    Сделал какую то логику

--- Figures/IsoscelesTriangleFigure.cs
+++ Figures/IsoscelesTriangleFigure.cs
@@ -9,9 +9,10 @@ namespace VectorDrawing.Figures
         {
             if (parameter.Points.Length == 1)
             {
-                Point[] tmp = new Point[parameter.Points.Length+1];
+                Point[] tmp = new Point[parameter.Points.Length+2];
                 Array.Copy(parameter.Points, tmp, parameter.Points.Length);
                 tmp[tmp.Length - 1] = parameter.TemporaryPoint;
+                tmp[tmp.Length - 2] = new Point(tmp[2].X+tmp[2].X*2,tmp[2].Y);
                 return tmp;
             }
             else
