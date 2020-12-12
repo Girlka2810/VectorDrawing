01f044771c39a086ac782e6d8ee8993d00fbeb2b
Чагай Сергей <frei.mizantrop@gmail.com>
2020-12-12 19:25:45 +0300

    конфликты

--- VectorDrawin.Tests/FiguresTests/RegularPolygonFigureTests.cs
+++ VectorDrawin.Tests/FiguresTests/RegularPolygonFigureTests.cs
@@ -14,7 +14,7 @@ namespace VectorDrawin.Tests.FiguresTests
     class RegularPolygonFigureTests
     {
         [Test, TestCaseSource(typeof(GetPointOfRegularPolygonMock))]
-        public void RegularPolygonFigureTest(int num_of_corners, PointF center, PointF crnt, PointF[] points)
+        public void RegularPolygonFigureTest(int num_of_corners, Point center, Point crnt, PointF[] points)
         {
             RegularPolygonFigure regularPolygonFigure = new RegularPolygonFigure();
             RegularPolygonParameter regularPolygonParameter = new RegularPolygonParameter
@@ -30,14 +30,14 @@ namespace VectorDrawin.Tests.FiguresTests
     }
     class GetPointOfRegularPolygonMock : IEnumerable
     {
-         public IEnumerator GetEnumerator()
+        public IEnumerator GetEnumerator()
         {
-            yield return new object[] { 3, new PointF(10, 10), new PointF(20, 20), new PointF[] { new PointF(10, 0),
-            new PointF((float)1.339746, 15), new PointF((float)18.6602535, 15)} };
-            yield return new object[] {4, new PointF(10, 10), new PointF(20, 20), new PointF[] { new PointF(10, 0), new PointF(0, 10),
-            new PointF(10,20), new PointF(20, 10)} };
-            //yield return new object[] {5, new PointF(10, 10), new PointF(20, 20), new PointF[] { new PointF(10, 0), new PointF((float)0.4894348, (float)6.90983),
-            //new PointF(15, 18), new PointF(4, 18), new PointF(0, 6) } };
+            yield return new object[] { 3, new Point(10, 10), new Point(20, 20), new Point[] { new Point(10, 0),
+            new Point(18, 14), new Point(1, 15) } };
+            yield return new object[] {4, new Point(10, 10), new Point(20, 20), new Point[] { new Point(10, 0), new Point(20, 10),
+            new Point(10,20), new Point(0, 10)} };
+            yield return new object[] {5, new Point(10, 10), new Point(20, 20), new Point[] { new Point(10, 0), new Point(19, 6),
+            new Point(15, 18), new Point(4, 18), new Point(0, 6) } };
         }
     }
 }
