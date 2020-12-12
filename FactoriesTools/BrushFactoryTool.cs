437859bdb5489be858c1f58dd9432ae5a7097a92
Kirill Kireev <kereeevkirill@gmail.com>
2020-12-13 02:21:38 +0300

    Add FactoryTool (Brush,Line<Nline,Rectangle,Square,Circle)

---
+++ FactoriesTools/BrushFactoryTool.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VectorDrawing.Tools;
+using VectorDrawing.Tools.Brushes;
+
+namespace VectorDrawing.FactoriesTools
+{
+    class BrushFactoryTool: IFactoryTool
+    {
+        
+        public AbstractTool Create(Pen pen)
+        {
+            return new BasicBrush(pen);
+        }
+    }
+}
