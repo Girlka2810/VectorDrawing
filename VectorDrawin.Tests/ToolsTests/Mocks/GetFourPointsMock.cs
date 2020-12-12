b466dcf7e6d1beadf0d5761774ec7151dce0052f
Чагай Сергей <frei.mizantrop@gmail.com>
2020-12-13 01:42:52 +0300

    Add Mocks

---
+++ VectorDrawin.Tests/ToolsTests/Mocks/GetFourPointsMock.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Drawing;
+
+
+namespace VectorDrawin.Tests.ToolsTests
+{
+    class GetFourPointsMock: IEnumerable
+    {
+        public IEnumerator GetEnumerator()
+        {
+            yield return new object[]
+          {
+                new List<PointF>
+                {
+                    new PointF(21, 68),
+                    new PointF(20, 20),
+                    new PointF(20, 20),
+                    new PointF(3, 45)
+                }
+          };
+        }
+    }
+}
--- VectorDrawin.Tests/ToolsTests/Mocks/GetPointsMock.cs
+++ VectorDrawin.Tests/ToolsTests/Mocks/GetPointsMock.cs
@@ -16,8 +16,7 @@ namespace VectorDrawin.Tests.ToolsTests
             {
                 new List<PointF>
                 {
-                    new PointF(10, 10),
-                    new PointF(20, 20)
+                   
                 }
             };
             yield return new object[]
@@ -25,7 +24,6 @@ namespace VectorDrawin.Tests.ToolsTests
                 new List<PointF>
                 {
                     new PointF(1, 1),
-                    new PointF(20, 20)
                 }
             };
             yield return new object[]
@@ -36,22 +34,6 @@ namespace VectorDrawin.Tests.ToolsTests
                     new PointF(1, 1)
                 }
             };
-            yield return new object[]
-           {
-                new List<PointF>
-                {
-                    new PointF(20, 50),
-                    new PointF(30, 15)
-                }
-           };
-            yield return new object[]
-           {
-                new List<PointF>
-                {
-                    new PointF(21, 68),
-                    new PointF(3, 45)
-                }
-           };
         }
     }
 }
