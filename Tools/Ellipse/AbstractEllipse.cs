db9bf8b524ec3bd95f1dab3da910100b93d05bad
Чагай Сергей <frei.mizantrop@gmail.com>
2020-12-14 19:19:01 +0300

    Add Convert from rectangle to ponts method for ellipse

--- Tools/Ellipse/AbstractEllipse.cs
+++ Tools/Ellipse/AbstractEllipse.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Drawing;
+using VectorDrawing.Figures;
+using VectorDrawing.Figures.Returns;
 
 namespace VectorDrawing.Tools.Ellipse
 {
@@ -9,6 +11,7 @@ namespace VectorDrawing.Tools.Ellipse
         public PointF Center { get; private set; }
         public float Width { get; set; }
         public float Height { get; set; }
+        private IFigure _figure;
 
 
         public AbsractEllipse(List<PointF> points, Pen pen) : base(pen)
@@ -35,6 +38,19 @@ namespace VectorDrawing.Tools.Ellipse
             Center = Points[0];
         }
 
-
+        protected PointF[] ConvertFromRectangleToPoints(RectangleF rect)
+        {
+            PointF[] arr = new PointF[4];
+            arr[0] = rect.Location;
+            arr[1] = new PointF(rect.Location.X + rect.Width, rect.Location.Y);
+            arr[2] = new PointF(rect.Location.X, rect.Location.Y + rect.Height);
+            arr[3] = new PointF(rect.Location.X + rect.Width, rect.Location.Y + rect.Height);
+            return arr;
+        }
+        public override void SavePoints()
+        {
+            EndShapePoints = ConvertFromRectangleToPoints(((EllipseReturn)
+                _figure.Get(GenerateParametrs())).Rectangle);
+        }
     }
 }
